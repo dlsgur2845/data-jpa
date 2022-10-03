041bb4b8679a6c500345195150c5eb1ee3f4a6da
inhyuk <dlsgur2845@naver.com>
2022-10-03 18:00:54 +0900

    메소드 이름으로 쿼리생성

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -2,6 +2,7 @@ package study.datajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.repository.MemberRepository;
 
 @RestController
 public class HelloController {
